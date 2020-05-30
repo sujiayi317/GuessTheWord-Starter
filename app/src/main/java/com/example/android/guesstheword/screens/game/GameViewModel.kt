d06621984f79c63f8ff05f1ccb84342600516195
sujiayi317 <sujiayi317@gmail.com>
2020-05-30 12:10:32 -0400

    Add LiveData to the GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,18 +1,21 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = ""
+    val word = MutableLiveData<String>()
     // The current score
-    var score = 0
+    val score = MutableLiveData<Int>()
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
+        word.value = ""
+        score.value = 0
         resetList()
         nextWord()
         Log.i("GameViewModel", "GameViewModel created!")
@@ -36,7 +39,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (wordList.isNotEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
 //        updateWordText()
 //        updateScoreText()
@@ -44,12 +47,14 @@ class GameViewModel : ViewModel() {
 
     /** Methods for buttons presses **/
     fun onSkip() {
-        score--
+//        score--
+        score.value = (score.value)?.minus(1)  // performs the subtraction with null-safety
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+//        score++
+        score.value = (score.value)?.plus(1)
         nextWord()
     }
 
